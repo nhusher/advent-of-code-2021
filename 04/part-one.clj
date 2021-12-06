7b50727fffc214caa27f93caf14101f5acc3fa21
nicholas husher <nhusher@gmail.com>
2021-12-06 11:47:54 -0500

    part 4 done

--- 04/part-one.clj
+++ 04/part-one.clj
@@ -1,10 +1,7 @@
 (ns part-one
   (:require [clojure.java.io :as io]
             [clojure.string  :as s]
-            [clojure.set     :refer [superset?]]))
-
-
-(def lines (line-seq (io/reader *in*)))
+            [clojure.set     :refer [superset? difference]]))
 
 ;;
 ;; Parses the input format into usable data:
@@ -13,7 +10,7 @@
   (map parse-long (s/split command-str #",")))
 
 (defn parse-board [board]
-  (map #(into #{} (map parse-long (s/split (s/trim %) #"\s+"))) board))
+  (map #(map parse-long (s/split (s/trim %) #"\s+")) board))
 
 (defn parse-boards [boards]
   (->> boards
@@ -26,10 +23,7 @@
         boards   (parse-boards rest)]
     [numbers boards]))
 
-
-(def board (first (second (parse-input lines))))
-
-(defn rotate [l] (apply map (comp set vector) l))
+(defn rotate [l] (apply map vector l))
 
 ;; Given a board and a list of called bingo numbers,
 ;; return true if this board will have won using those numbers
@@ -37,7 +31,58 @@
   (let [board-r    (rotate board)
         number-set (set numbers)]
     (or
-      (some #(superset? number-set %) board)
-      (some #(superset? number-set %) board-r))))
+      (some #(superset? number-set (set %)) board)
+      (some #(superset? number-set (set %)) board-r))))
 
-(parse-input lines)
+;; Return a seq of the unmarked cells in a bingo card, given
+;; a list of numbers that have been called. Used for generating
+;; a final score.
+(defn unmarked [numbers board]
+  (let [number-set (set numbers)]
+    (mapcat #(difference (set %) number-set) board)))
+
+;; Generate a seq of which boards have won, which have not yet won
+;; and what numbers have been called. Winners and and non-winners are
+;; stored as sets.
+(defn winner-seq [numbers boards]
+  (map
+    (fn [n]
+      (let [nums (take n numbers)]
+        { :numbers nums, 
+          :winning-boards (set (filter #(winner? nums %) boards))
+          :remaining-boards (set (remove #(winner? nums %) boards))}))
+    (range 1 (inc (count numbers)))))
+
+;; Yeild the first winner from the winner seq:
+(defn first-winner [winner-seq]
+   (->> winner-seq
+        (filter #(not= #{} (:winning-boards %)))
+        first))
+
+;; Generate a score given a single board 
+(defn generate-score [winner]
+  (let [{ boards :winning-boards, nums :numbers } winner]
+    (* (last nums) (apply + (unmarked nums (first boards))))))
+
+;; Read lines, convert to seq:
+(let [[numbers boards] (parse-input (line-seq (io/reader *in*)))]
+  (def winners (winner-seq numbers boards)))
+
+(prn " part 1:"
+  (-> winners
+      first-winner
+      generate-score))
+
+(defn last-winner [winner-seq]
+  (let [final-pair (->> winner-seq
+                        (partition 2 1)
+                        (filter #(= (count (:remaining-boards (second %))) 0))
+                        first)
+        final-nums (:numbers (second final-pair))
+        final-board (:remaining-boards (first final-pair))]
+    { :numbers final-nums, :winning-boards final-board }))
+
+(prn "part 2:"
+  (-> winners
+      last-winner
+      generate-score))
